24820e27ca85ed12f189dfd687ab5c4464345607
William Stucker <william.stucker@lightelligence.ai>
2020-02-25 20:34:52 -0500

    Change extract to decode to be more inline with protobuf documentation.

--- pb_pkg.svh
+++ pb_pkg.svh
@@ -12,7 +12,7 @@ package pb_pkg;
    // _stream is not modified, passed as ref for performance
    // _cursor is advanced to next unconsumed byte in stream
    // https://developers.google.com/protocol-buffers/docs/encoding#varints
-   function automatic bit extract_varint(output longint unsigned _varint,
+   function automatic bit decode_varint(output longint unsigned _varint,
                                          ref byte         _stream[],
                                          ref int unsigned _cursor);
       int bit_counter = 0;
@@ -26,19 +26,19 @@ package pb_pkg;
          bit_counter+=7;
       end
       return 1;
-   endfunction : extract_varint
+   endfunction : decode_varint
 
    // https://developers.google.com/protocol-buffers/docs/encoding#structure
-   function automatic bit extract_message_key(output int unsigned _field_number,
+   function automatic bit decode_message_key(output int unsigned _field_number,
                                               output int unsigned _wire_type
                                               ref byte            _stream[],
                                               ref int unsigned    _cursor);
       int unsigned varint;
       bit          retval = 0;
-      retval = extract_varint(._varint(varint), ._stream(_stream), ._cursor(_cursor));
+      retval = decode_varint(._varint(varint), ._stream(_stream), ._cursor(_cursor));
       _field_number = varint >> 3;
       _wire_type = varint & 3'b111;
       return retval;
-   endfunction : extract_message_key
+   endfunction : decode_message_key
 
 endpackage : pb_pkg
--- test/pb_pkg_test.sv
+++ test/pb_pkg_test.sv
@@ -4,7 +4,7 @@ function automatic void test_varint_single_byte();
    byte stream[] = '{8'b0000_0001};
    int unsigned cursor = 0;
    longint          result;
-   bit failure = pb_pkg::extract_varint(result, stream, cursor);
+   bit failure = pb_pkg::decode_varint(result, stream, cursor);
    assert (failure == 0);
    assert (result == 1) else $display("result: %0d", result);
 endfunction : test_varint_single_byte
@@ -13,7 +13,7 @@ function automatic void test_varint_300();
    byte    stream[] = '{8'b1010_1100, 8'b0000_0010};
    int unsigned cursor = 0;
    longint          result;
-   bit failure = pb_pkg::extract_varint(result, stream, cursor);
+   bit failure = pb_pkg::decode_varint(result, stream, cursor);
    assert (failure == 0);
    assert (result == 300) else $display("result: %0d", result);
 endfunction : test_varint_300
@@ -22,7 +22,7 @@ function automatic void test_message_key();
    byte stream[] = '{8'b0000_1000};
    int unsigned cursor = 0;
    int unsigned field_number, wire_type;
-   bit failure = pb_pkg::extract_message_key(field_number, wire_type, stream, cursor);
+   bit failure = pb_pkg::decode_message_key(field_number, wire_type, stream, cursor);
    assert (failure == 0);
    assert (field_number == 1) else $display("field_number: %0d", field_number);
    assert (wire_type == 0) else $display("wire_type: %0d", wire_type);
