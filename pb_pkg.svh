ec795715447c4303550482c3c44be57cc062a2ee
William Stucker <william.stucker@lightelligence.ai>
2020-02-25 20:33:32 -0500

    Add message key extraction

--- pb_pkg.svh
+++ pb_pkg.svh
@@ -11,7 +11,10 @@ package pb_pkg;
    // Given a byte stream, start at _position cursor and extract a varint
    // _stream is not modified, passed as ref for performance
    // _cursor is advanced to next unconsumed byte in stream
-   function automatic bit extract_varint(output longint unsigned _varint, ref byte _stream[], ref int unsigned _cursor);
+   // https://developers.google.com/protocol-buffers/docs/encoding#varints
+   function automatic bit extract_varint(output longint unsigned _varint,
+                                         ref byte         _stream[],
+                                         ref int unsigned _cursor);
       int bit_counter = 0;
       _varint = 0;
       for (int unsigned ii=0; ii < MAX_VARINT_BYTES; ii++) begin
@@ -24,5 +27,18 @@ package pb_pkg;
       end
       return 1;
    endfunction : extract_varint
+
+   // https://developers.google.com/protocol-buffers/docs/encoding#structure
+   function automatic bit extract_message_key(output int unsigned _field_number,
+                                              output int unsigned _wire_type
+                                              ref byte            _stream[],
+                                              ref int unsigned    _cursor);
+      int unsigned varint;
+      bit          retval = 0;
+      retval = extract_varint(._varint(varint), ._stream(_stream), ._cursor(_cursor));
+      _field_number = varint >> 3;
+      _wire_type = varint & 3'b111;
+      return retval;
+   endfunction : extract_message_key
 
 endpackage : pb_pkg
--- test/pb_pkg_test.sv
+++ test/pb_pkg_test.sv
@@ -18,11 +18,20 @@ function automatic void test_varint_300();
    assert (result == 300) else $display("result: %0d", result);
 endfunction : test_varint_300
 
+function automatic void test_message_key();
+   byte stream[] = '{8'b0000_1000};
+   int unsigned cursor = 0;
+   int unsigned field_number, wire_type;
+   bit failure = pb_pkg::extract_message_key(field_number, wire_type, stream, cursor);
+   assert (failure == 0);
+   assert (field_number == 1) else $display("field_number: %0d", field_number);
+   assert (wire_type == 0) else $display("wire_type: %0d", wire_type);
+endfunction : test_message_key
 
 module tb_top;
    initial begin
       test_varint_single_byte();
       test_varint_300();
+      test_message_key();
    end
-   
 endmodule : tb_top
