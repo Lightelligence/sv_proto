dede2ab1a776294c6156e025eb71faedff02a09a
William Stucker <william.stucker@lightelligence.ai>
2020-02-25 19:49:35 -0500

    Refactor to give failure indicator. Also switch to int unsigned for cursor to avoid warning about truncation

--- pb_pkg.svh
+++ pb_pkg.svh
@@ -11,17 +11,18 @@ package pb_pkg;
    // Given a byte stream, start at _position cursor and extract a varint
    // _stream is not modified, passed as ref for performance
    // _cursor is advanced to next unconsumed byte in stream
-   function automatic longint unsigned extract_varint(ref byte _stream[], ref longint unsigned _cursor);
+   function automatic bit extract_varint(output longint unsigned _varint, ref byte _stream[], ref int unsigned _cursor);
       int bit_counter = 0;
-      extract_varint = 0;
+      _varint = 0;
       for (int unsigned ii=0; ii < MAX_VARINT_BYTES; ii++) begin
          byte current_byte = _stream[_cursor++];
-         extract_varint |= (current_byte & 8'h7f) << bit_counter;
+         _varint |= (current_byte & 8'h7f) << bit_counter;
          if (current_byte[7] == 0) begin
-            break;
+            return 0;
          end
          bit_counter+=7;
       end
+      return 1;
    endfunction : extract_varint
 
 endpackage : pb_pkg
--- test/pb_pkg_test.sv
+++ test/pb_pkg_test.sv
@@ -2,15 +2,19 @@
 
 function automatic void test_varint_single_byte();
    byte stream[] = '{8'b0000_0001};
-   longint unsigned cursor = 0;
-   longint result = pb_pkg::extract_varint(stream, cursor);
+   int unsigned cursor = 0;
+   longint          result;
+   bit failure = pb_pkg::extract_varint(result, stream, cursor);
+   assert (failure == 0);
    assert (result == 1) else $display("result: %0d", result);
 endfunction : test_varint_single_byte
 
 function automatic void test_varint_300();
    byte    stream[] = '{8'b1010_1100, 8'b0000_0010};
-   longint unsigned cursor = 0;
-   longint result = pb_pkg::extract_varint(stream, cursor);
+   int unsigned cursor = 0;
+   longint          result;
+   bit failure = pb_pkg::extract_varint(result, stream, cursor);
+   assert (failure == 0);
    assert (result == 300) else $display("result: %0d", result);
 endfunction : test_varint_300
 
